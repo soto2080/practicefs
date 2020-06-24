06eca7f602772fb6f4cb88408565b02d595d5f5a
Ming-Han Yang <soto2080@gmail.com>
2020-06-25 00:43:37 +0800

    pseudo data structure

--- practicefs.cpp
+++ practicefs.cpp
@@ -6,9 +6,35 @@
 
 #include "practicefs.h"
 #include <iostream>
+#include <bitset>
+#include <string.h>
 
+// Global variables
+struct superblock sb;
+std::bitset<IMAP_SIZE> imap(0);
+std::bitset<DMAP_SIZE> dmap(0);
+struct datablock blocks[DMAP_SIZE];
+struct inode inodes[IMAP_SIZE];
 
+// Functions
+void * op_init(struct fuse_conn_info *conn, struct fuse_config *config){
+	// Init superblock
+	sb.blk_size = BLK_SIZE;
+	sb.fs_size = 0;
+	sb.imap_size = IMAP_SIZE;
+	sb.num_free_inode = IMAP_SIZE;
+	sb.dmap_size = DMAP_SIZE;
+	sb.num_free_dblk = DMAP_SIZE;
+	sb.name_size = NAME_LENGTH;
+	sb.cur_inode = 0;
+	std::cout<<"Init SuperBlock"<<std::endl;
+	//memset(blocks, 0, sizeof(blocks) * sb.dmap_size);
+	//memset(inodes, 0, sizeof(inodes) * sb.imap_size);
+}
+
+// VFS Operations
 static struct fuse_operations practicefs_op = {
+	.init		= op_init,
 };
 
 int main( int argc, char *argv[] )
--- practicefs.h
+++ practicefs.h
@@ -1,9 +1,101 @@
 #ifndef PRACTICEFS
 
 #define PRACTICEFS
-
 #define FUSE_USE_VERSION 31
 
 #include <fuse.h>
+#include <stdint.h>
+
+
+/*
+Bitmaps:
+*/
+
+const int IMAP_SIZE = 256;
+const int DMAP_SIZE = 512;
+
+/*
+DataBlock:
+*/
+
+const int BLK_SIZE = 512;
+const int PG_SIZE = 8 * BLK_SIZE;
+
+struct datablock {
+    char data[BLK_SIZE];
+};
+
+/*
+Inode:
+*/
+
+/* File types. */
+enum INODE_TYPE {
+    IFIFO,
+    IFCHR,
+    IFDIR,
+    IFBLK,
+    IFREG,
+    IFLNK,
+    IFSOCK
+};
+
+/*
+ * Special inode numbers
+ */
+#define	EXT2_BAD_INO		 1	/* Bad blocks inode */
+#define EXT2_ROOT_INO		 2	/* Root inode */
+#define EXT2_BOOT_LOADER_INO	 5	/* Boot loader inode */
+#define EXT2_UNDEL_DIR_INO	 6	/* Undelete directory inode */
+
+/* First non-reserved inode for old ext2 filesystems */
+#define EXT2_GOOD_OLD_FIRST_INO	11
+
+/*
+ * Constants relative to the data blocks
+ */
+#define	EXT2_NDIR_BLOCKS		12
+#define	EXT2_IND_BLOCK			EXT2_NDIR_BLOCKS
+#define	EXT2_DIND_BLOCK			(EXT2_IND_BLOCK + 1)
+#define	EXT2_TIND_BLOCK			(EXT2_DIND_BLOCK + 1)
+#define	EXT2_N_BLOCKS			(EXT2_TIND_BLOCK + 1)
+
+struct inode {
+    enum INODE_TYPE i_type; /* What kind of file this inode point to */
+    uint32_t		i_nlink;	/* File link count. */
+	uint32_t	    i_uid;		/* File owner. */
+	uint32_t	    i_gid;		/* File group. */
+	uint64_t	    i_size;		/* File byte count. */
+	uint64_t	    i_blocks;	/* Blocks actually held. */
+    uint32_t        i_number;   /* The identity of the inode. */
+    struct datablock  *i_block[EXT2_N_BLOCKS]; /* pointer to datablock */
+    // Time Related
+};
+
+/*
+SuperBlock:
+*/
+#define NAME_LENGTH 256
+struct superblock {
+    uint32_t imap_size;
+    uint32_t dmap_size;
+    uint32_t blk_size;
+    uint32_t num_blks;
+    uint64_t fs_size;
+    uint32_t name_size;
+    uint32_t cur_inode;
+    uint32_t num_free_inode;
+    uint32_t num_free_dblk;
+};
+
+/*
+Others:
+*/
+
+
+struct directory_entry {
+  int inode_num;
+  char file_name[NAME_LENGTH];
+};
 
 #endif
